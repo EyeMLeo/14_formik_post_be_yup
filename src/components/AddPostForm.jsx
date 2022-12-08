f01059fc07e26b988d8c031b062de4f684bf82dc
EyeMLeo <Leonid.Akuta@gmail.com>
2022-12-08 21:22:09 +0100

    14_formik_POST

--- src/components/AddPostForm.jsx
+++ src/components/AddPostForm.jsx
@@ -1,8 +1,10 @@
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import InputError from './InputError';
+import { Redirect, useHistory } from 'react-router-dom';
 
 function AddPostForm(props) {
+  let history = useHistory();
   const formik = useFormik({
     initialValues: {
       image: '',
@@ -26,37 +28,50 @@ function AddPostForm(props) {
     onSubmit: (values) => {
       console.log('values ===', values);
 
+      history.push('/posts');
       // alert(JSON.stringify(values, null, 2));
 
-      // sutvarkyti tags
+      // **********************************************************************************
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          body: values.body,
+          image: values.image,
+          reaction: values.reactions,
+          tags: values.tags,
+          title: values.title,
+          userId: values.userId,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log('Sekme json ===', json);
+          history.push('/posts');
+        })
+        .catch((error) => {
+          onsole.log('Nesekme - error siunciant duomenis ??? ' + error);
+        });
 
-      // siusti duomenis su fetch
-      // sendDataFetch(values)
+      ////////////////////////////////////////////////////////////////////////////////////////
+
+      //??? sendDataFetch(values)
       // jei sekmingai nusiuntem tai console log sekme
-      // mes norim naviguoti i PostsPage su react-router is AddPostsPage
       // jei ne tai nesekme
+
+      // jei klaida ateina response? nepagauna klaidos?
+      // toliau generuoti is response papildoma kortele?
+      // redirect vyksta ir su klaida
     },
   });
-  /*
-  reikalingi input
-  "image" text
-  "title" text
-  "body" textarea
-  "reactions" number 0
-  "userId"
-  */
 
-  // console.log('formik.values ===', formik.values);
-
-  // sudeti likusiems inputams klaidu atvaizdavima
-  // extra prideti inputui klase inputErrorField jei jame yra klaida
-  // console.log('formik.errors ===', formik.errors);
-  // console.log('formik.touched ===', formik.touched);
   return (
     <div>
       <h2>Create post</h2>
 
-      <form onSubmit={formik.handleSubmit} className='card'>
+      <form onSubmit={formik.handleSubmit} className="card">
         <input
           className={
             formik.touched.image && formik.errors.image ? 'inputErrorField' : ''
@@ -64,9 +79,9 @@ function AddPostForm(props) {
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.image}
-          type='text'
-          placeholder='Image'
-          name='image'
+          type="text"
+          placeholder="Image"
+          name="image"
         />
         <InputError error={formik.errors.image} touch={formik.touched.image} />
         {/* sukurti InputError componenta kuris gaves props error, atvaizduos klaidos p taga */}
@@ -78,9 +93,9 @@ function AddPostForm(props) {
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.title}
-          type='text'
-          placeholder='Title'
-          name='title'
+          type="text"
+          placeholder="Title"
+          name="title"
         />
 
         {/* level2 error <InputError formik={formik} field={'title'} /> */}
@@ -92,11 +107,11 @@ function AddPostForm(props) {
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.body}
-          name='body'
-          placeholder='Your text'
+          name="body"
+          placeholder="Your text"
         ></textarea>
         {formik.touched.body && formik.errors.body && (
-          <p className='inputErroMsg'>{formik.errors.body}</p>
+          <p className="inputErroMsg">{formik.errors.body}</p>
         )}
         <input
           className={
@@ -107,12 +122,12 @@ function AddPostForm(props) {
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.reactions}
-          type='number'
-          placeholder='Reactions'
-          name='reactions'
+          type="number"
+          placeholder="Reactions"
+          name="reactions"
         />
         {formik.touched.reactions && formik.errors.reactions && (
-          <p className='inputErroMsg'>{formik.errors.reactions}</p>
+          <p className="inputErroMsg">{formik.errors.reactions}</p>
         )}
         {/* prideti dar viea inputa
         text, jame validacija min 3 raides
@@ -125,11 +140,11 @@ function AddPostForm(props) {
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.userId}
-          type='number'
-          name='userId'
+          type="number"
+          name="userId"
           disabled
         />
-        <button type='submit'>Create</button>
+        <button type="submit">Create</button>
       </form>
     </div>
   );
